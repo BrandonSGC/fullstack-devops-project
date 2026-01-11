ad75603623cc832063c7a942bbecd6626f8c2ee4
BrandonSGC <brangmz20@gmail.com>
2026-01-11 14:34:09 -0600

    refactor(infra): rename Key Vault and related variables

--- infra/modules/keyvault/main.tf
+++ infra/modules/keyvault/main.tf
@@ -3,7 +3,7 @@ data "azurerm_client_config" "current" {}
 
 # Create an Azure Key Vault
 resource "azurerm_key_vault" "keyvault" {
-  name                        = "kv-fullstack-${var.environment}-bgc3"
+  name                        = "kv-${var.name}-${var.environment}"
   location                    = var.location
   resource_group_name         = var.rg_name
   enabled_for_disk_encryption = true
